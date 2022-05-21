cb00e56c3ffea5952252466c245069d1549019cd
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-05-21 18:38:04 +0530

    dfgjhg

--- account/index.php
+++ account/index.php
@@ -52,6 +52,18 @@ session_start();
         <!-- <div class="linkTwo"><span class="links">Follow</span></div> -->
       </div>
     </div>
+    <div class="featuredArticle">
+      <span class="title">Featured Article</span>
+      <div class="featuredPost">
+        <span class="post">Hi Guys What are you doing I am here to make an egg</span>
+        <div class="fpdetail">
+          <span class="channelName">Shafiq Hub</span>
+          <span class="fpDot">&#x2022;</span>
+          <span class="pubTime">1 hour Ago</span>
+        </div>
+      </div>
+    </div>
+
     <div class="channelsDiv">
       <span class="title">Channels</span>
       <div class="channel">
@@ -78,40 +90,69 @@ session_start();
       </div>
     </div>
 
-    <div class="popularArticles">
-      <span class="title">Populer Articles</span>
-      <div class="articlesDiv">
-        <div class="article">
-          <div class="image">
-            <img src="../uploads/posts/2022/7/67895436.jpg" alt="">
-          </div>
-          <div class="nameAndTime">
-          </div>
-          <div class="articleTitle">
-            <span>The Most  Places In the World</span>
-          </div>
-        </div>
-        <div class="article">
-          <div class="image">
-            <img src="" alt="">
-          </div>
-          <div class="nameAndTime">
-          </div>
-          <div class="articleTitle">
-            <span>The Most Beautiful Places In the World</span>
-          </div>
-        </div>
-        <div class="article">
-          <div class="image">
-            <img src="" alt="">
-          </div>
-          <div class="nameAndTime">
-          </div>
-          <div class="articleTitle">
-            <span>The Most Beautiful Places In the World</span>
-          </div>
-        </div>
 
+    <div class="popularArticles">
+      <div class="top">
+        <select class="title select" name="filterOpt" id="filterOpt">
+          <option value="">Latest </option>
+          <option value="trending">Popular</option>
+          <option value="newlyAdded">Newest</option>
+        </select>
+        <span class="title">Articles</span>
+      </div>
+      <div class="posts cont500">
+        <div class="postBody">
+
+          <div class="postPic"> <img src="/uploads/posts/2022/7/12095427.jpg" alt=""> </div>
+          <div class="postTitle"><a href="" id="postTitle" href=""> How to get a full detail of your</a></div>
+          <div class="extFoot">
+            <span class="meta"><a id="channelName"  href="">Shafiq Hub</a></span>
+            <p class="dot">&#x2022;</p>
+            <span   class="meta"><a id="authorName" href="">Mohd Shafiq</a></span>
+            <p class="dot">&#x2022;</p>
+            <span id="pubTime" class="meta">1hr Ago</span>
+          </div>
+        </div>
+        <div class="postFooter">
+          <div class="footItems" id="react">
+            <div id="like" class="react"><img  class="footImages"  src="/assets/pics/svgs/thumbs-up.svg" alt=""></div>
+            <div id="likeCount" class="react rt footImages fontFam b sm ml_d4em">1</div>
+          </div>
+          <div class="footItems" >
+            <img id="comment"class="footImages"  src="/assets/pics/svgs/comment_notFilled_2.svg" alt="">
+            <div id="comentCount" class=" react rt footImages fontFam b sm ml_d4em">13</div>
+          </div>
+          <div class="footItems" id="share">
+            <img  class="footImages" src="/assets/pics/svgs/share_en.svg" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="posts cont500">
+        <div class="postBody">
+
+          <div class="postPic"> <img src="/uploads/posts/2022/7/12095427.jpg" alt=""> </div>
+          <div class="postTitle"><a href="" id="postTitle" href=""> How to get a full detail of your</a></div>
+          <div class="extFoot">
+            <span class="meta"><a id="channelName"  href="">Shafiq Hub</a></span>
+            <p class="dot">&#x2022;</p>
+            <span   class="meta"><a id="authorName" href="">Mohd Shafiq</a></span>
+            <p class="dot">&#x2022;</p>
+            <span id="pubTime" class="meta">1hr Ago</span>
+          </div>
+        </div>
+        <div class="postFooter">
+          <div class="footItems" id="react">
+            <div id="like" class="react"><img  class="footImages"  src="/assets/pics/svgs/thumbs-up.svg" alt=""></div>
+            <div id="likeCount" class="react rt footImages fontFam b sm ml_d4em">1</div>
+          </div>
+          <div class="footItems" >
+            <img id="comment"class="footImages"  src="/assets/pics/svgs/comment_notFilled_2.svg" alt="">
+            <div id="comentCount" class=" react rt footImages fontFam b sm ml_d4em">13</div>
+          </div>
+          <div class="footItems" id="share">
+            <img  class="footImages" src="/assets/pics/svgs/share_en.svg" alt="">
+          </div>
+        </div>
       </div>
     </div>
   </div>
