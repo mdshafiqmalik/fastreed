b8949a1d7bb2acb9f47fa874718f3bb710117c72
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-08 01:20:51 +0530

    ghjhgj

--- users/createUser.php
+++ users/createUser.php
@@ -27,7 +27,7 @@ if (isset($_POST)) {
           $to = "mdshafiqmalik98@gmail.com";
           $subject = "OTP Authenication";
           $headers = "From: support@earnmore.com" . "\r\n" ."CC: admin@shafiqhub.com";
-          // $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+          $headers .= 'Content-type: text/html';
 
           mail($to,$subject,$message,$headers);
         }
