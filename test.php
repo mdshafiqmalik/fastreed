929fc2816614918535a4c830bb1e95c8d29c01ec
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-10 22:21:19 +0530

    fghgfh

--- test.php
+++ test.php
@@ -2,9 +2,6 @@
   <head>
   <title>OTP Authenication</title>
   <style media='screen'>
-    #message{
-      font-size: 1.2em;
-    }
     #link{
       text-align:center;
       margin: .8em 0em;
@@ -24,7 +21,7 @@
       text-align:center;
       margin: .8em 0em;
     }
-    #OTP p{
+    #OTP code{
       font-size: 1.2em;
       padding: .4em 2em;
       background-color: #eee;
@@ -41,30 +38,35 @@
       background-color: #dee;
     }
     #copy{
-      margin:
+      margin-left: 5px;
+      letter-spacing: 1px;
+      font-size:.9em;
+      color: red;
     }
-    #b{
-      display: flex;
-      justify-content:  center;
-      align-items: center;
-      background-color: #eee;
+    #copy:hover{
+      cursor: pointer;
+    }
+    #message, #link{
+      font-size: 1.2em;
     }
     #cont{
       background-color: white;
       padding: .3em;
       max-width: 500px;
+      border: .5px solid #eee;
+      border-radius: 5px;
     }
   </style>
   </head>
-  <body id='b'>
+  <body>
     <div id='cont'>
       <div id='message'>
           Dear <b>".$userFullName." </b><br><br>
           One Time Password(OTP) for account verification is: <b>(valid for 10 minutes only)</b>
           <div id='OTP'>
-            <p>".$randOTP."
-              <span id ='copy'>copy</span>
-            </p>
+            <code id='cpOTP'>".$randOTP."
+              <span onclick='copyText()' id ='copy'>copy</span>
+            </code>
           </div>
           <div>Or you can verify your account by clicking on the link given
             <b>(valid for 10 minutes only)</b>
@@ -75,11 +77,24 @@
       </div>
       <footer id='footer'>
       This mail is sent to <b>".$userEmail." </b>and is intended for account verification of <b>".$userFullName."</b>. <br>Kindly ignore if you don't know about this.</b>
-      </footer><hr>
+      </footer><br><hr>
       <div>You can create an account with us by clicking on link given below
       </div>
+      <br>
       <div id='link'><a href='https://m.shafiqhub.com/register'> Sign Up With Fastreed</a>
       </div>
     </div>
+    <script type="text/javascript">
+    function copyText(){
+      var copyText = document.getElementById('cpOTP');
+      var cp = document.getElementById('copy');
+      copyText.select();
+      copyText.setSelectionRange(0, 99999); /* For mobile devices */
+      /* Copy the text inside the text field */
+      navigator.clipboard.writeText(copyText.value);
+      cp.innerHTML = 'copied';
+      cp.style.color= 'orange';
+    }
+    </script>
   </body>
 </html>
