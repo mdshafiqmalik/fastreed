31dd6eb981ce2abff434874b8bf4a2cb51dd8612
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-10 10:12:42 +0530

    dfghh

--- users/verify.php
+++ users/verify.php
@@ -37,7 +37,7 @@ session_start();
                 $_SESSION['uisnnue'] = $userID;
                 // setcookie('uisnnue', $encUID, time()+(86400*7), '/');
                 if (isset($_SESSION['uisnnue'])) {
-                  header("Location: ../profile/?eikooCtes=true");
+                  header("Location: ../profile?eikooCtes=true");
                 }else {
                   echo '<center><span id="successMessage">Registered Sucesssfully</span></center>';
                   echo '<center><span id="successMessage">Redirecting to Login page</span></center>';
