c62d0803c577c1a4acecf4e9191328024b0af344
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-10 17:54:48 +0530

    thythy

--- _.config/sjdhfjsadkeys.php
+++ _.config/sjdhfjsadkeys.php
@@ -1,11 +1,11 @@
 <?php
-$encryption_key = "yhen562febsubinsdybb9027";
+$encryption_key = "jh27grb091nbck90)3bv%faqikn456bc_@bvhj6gdv18vbgdtcv(%)";
 $ciphering = "AES-128-CTR";
 $iv_length = openssl_cipher_iv_length($ciphering);
 $options = 0;
 $encryption_iv = '1234567891011121';
 
 
-$decryption_key = "yhen562febsubinsdybb9027";
+$decryption_key = "jh27grb091nbck90)3bv%faqikn456bc_@bvhj6gdv18vbgdtcv(%)";
 $decryption_iv = '1234567891011121';
  ?>
