32b12de98bc6b4fb718f2f960c25eeb2e598e1b6
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-11 01:07:39 +0530

    fghgh

--- users/verify.php
+++ users/verify.php
@@ -17,12 +17,11 @@ if (isset($_GET['suid'])) {
               $_SESSION['uisnnue'] = $userID;
               // setcookie('uisnnue', $encUID, time()+(86400*7), '/');
               if (isset($_SESSION['uisnnue'])) {
-                $userID = $_SESSION['uisnnue'];
                 include 'otp.php';
                 include '../_.config/_s_db_.php';
                 $sql = "SELECT * FROM fast_users WHERE userID = '$userID";
                 $res = mysqli_query($db, $sql);
-                $row = $res-> fetch_assoc();
+                $row = $res->fetch_assoc();
                 $userFullNam = $row['userFullName'];
                 $userEmail = $row['userEmail'];
                 $userName = $row['userName'];
