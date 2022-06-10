47c08a08e472b32637ee34cf9d29009e9df66abe
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-10 11:40:26 +0530

    fdghgj

--- profile/index.php
+++ profile/index.php
@@ -1,123 +1,160 @@
 <?php
 session_start();
+if(isset($_COOKIE['uisnnue'])) {
+  if (!empty($_COOKIE['uisnnue'])) {
+    include '../_.config/sjdhfjsadkeys.php';
+    $dUserID = openssl_decrypt ($_COOKIE['uisnnue'], $ciphering,
+    $decryption_key, $options, $decryption_iv);
+    $_SESSION["uisnnue"] = $dUserID;
+    if (checkUserID($_SESSION["uisnnue"])) {
+      renderProfile($_SESSION['uisnnue']);
+    }else {
+      echo '<script type="text/javascript">
+        document.location = "../login/?code=001UNF";
+      </script>';
+    }
+  }else {
+    echo '<script type="text/javascript">
+      document.location = "../login/?code=002UNF";
+    </script>';
+  }
+
+}elseif (isset($_SESSION['uisnnue']) && isset($_GET['eikooCtes'])) {
+  if ((boolean)$_GET['eikooCtes']) {
+    if (checkUserID($_SESSION['uisnnue'])) {
+      $encUID = openssl_encrypt($_SESSION['uisnnue'], $ciphering,
+      $encryption_key, $options, $encryption_iv);
+      setcookie('uisnnue', $encUID, time()+(86400*30), '/');
+      $u = $_SESSION['uisnnue'];
+      renderProfile($u);
+    }else {
+      var_dump('user not exist');
+      // echo '<script type="text/javascript">
+      //   document.location = "../login?code=0003UNF";
+      // </script>';
+    }
+  }else {
+    if (checkUserID($_SESSION['uisnnue'])) {
+      renderProfile($_SESSION['uisnnue']);
+    }else {
+      echo '<script type="text/javascript">
+        document.location = "../login?code=004UNF";
+      </script>';
+    }
+  }
+}else {
+  echo '<script type="text/javascript">
+    document.location = "../login?code=00NCSS";
+  </script>';
+}
+
 $GLOBALS['containerStart'] = '<div id="" class="container">';
 $GLOBALS['divStop'] = '</div>';
-$selfProfile = '
-<!-- Self Profile Opened -->
-  <div class="authorProfile">
-    <div class="topDiv">
-      <div class="authorPic"> <img src="../uploads/users/2022/7/25316534.jpg" alt=""> </div>
-      <div class="authorDetails">
-        <div class="userNameWork">
-          <span id="userFullName">Jhon Doe</span>
-          <span id="userType">Administator</span>
-          <span class="designation">Alaska, USA</span>
-          <span class="designation">Joined 23-02-2022</span>
-        </div>
-        <div class="userParam">
-          <div class="userArticles">
-            <span class="userParameters">Articles</span>
-            <span class="values">239</span>
-          </div>
-          <div class="userFollowers">
-            <span class="userParameters">Follows</span>
-            <span class="values">12M</span>
-          </div>
-          <div class="userRating">
-            <span class="userParameters">Rating</span>
-            <span class="values">9.8</span>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="bottomDiv">
-      <div id="linkOne"class="linkOne"> <span class="links"> <a href="#">Create</a></span> </div>
-      <div id="linkTwo" class="linkOne"> <span class="links"> <a href="#">Settings</a> </span> </div>
-    </div>
-  </div>
-<!-- Self Profile Closed -->';
-$featuredArticle = '<!-- Featured Article Opened -->
-  <div class="featuredArticle">
-    <span class="title">Featured Article</span>
-    <div class="featuredPost">
-      <span class="post">Hi Guys What are you doing I am here to make an egg</span>
-      <div class="fpdetail">
-        <span class="channelName">Fast Hub</span>
-        <span class="fpDot">&#x2022;</span>
-        <span class="pubTime">1 hour Ago</span>
-      </div>
-    </div>
-  </div>
-<!-- Featured Article Closed -->';
-$channels = '<!-- Channels Open -->
-  <div class="channelsDiv">
-    <span class="title">Channels</span>
-    <div class="channel">
-      <div class="channels">
-        <div class="channelImg">
-          <img src="../uploads/posts/2022/7/67895436.jpg" alt=" ">
-        </div>
-        <div class="channelDetail">
-          <span class="channelName">Dream Hub</span>
-          <span class="articlesCount">1.6K Articles</span>
-          <div class="channelButtons"> <a href="#">Subscribe</a> </div>
-        </div>
-      </div>
-      <div class="channels">
-        <div class="channelImg">
-          <img src="../uploads/posts/2022/7/67529813.jpg" alt=" ">
-        </div>
-        <div class="channelDetail">
-          <span class="channelName">The Living Society</span>
-          <span class="articlesCount">160 Articles</span>
-          <div class="channelButtons"> <a href="#">Subscribe</a> </div>
-        </div>
-      </div>
-    </div>
-  </div>
-<!-- Channels Close -->';
+function checkUserID($dUserID){
+  include '../_.config/_s_db_.php';
+  $link = new mysqli("$hostName","$userName","$passWord","$dbName");
+  $checkUserID = "SELECT userID FROM fast_users Where userID = '$dUserID'";
+  $userDat = mysqli_query($link, $checkUserID);
+  if (mysqli_num_rows($userDat)) {
+    $exist = true;
+  }else {
+    setcookie("uisnnue", "", time()-3600);
+    unset($_SESSION['uisnnue']);
+    $exist = false;
+  }
+  return $exist;
+}
+function renderProfile($UID){
+  include '../_.config/_s_db_.php';
+  $link = new mysqli("$hostName","$userName","$passWord","$dbName");
+  $getUserData = "SELECT * FROM user_credentials Where userID = '$UID'";
+  $result = mysqli_query($db,$getUserData);
+  if (mysqli_num_rows($result)) {
+    $row = $result->fetch_assoc();
+    $userFullName = $row['userFullName'];
+    $userJoinDate = $row['userJoiningDate'];
+    $userCountry = $row['userCountry'];
+    switch ($row['userType']) {
+      case '0':
+        $userType = "Reader";
+        break;
+      case '1':
+        $userType = "Author";
+        break;
+      case '2':
+        $userType = "Administator";
+        break;
+      default:
+        $userType = "Reader";
+        break;
+    }
+    $getPost = "SELECT * FROM fast_posts WHERE userID ='$UID'";
+    $postData = mysqli_query($link, $getPost);
+    $postsCount = mysqli_num_rows($postData);
 
-$articles = '<!-- Popular Articles Open -->
-  <div class="popularArticles">
-    <div class="top">
-      <select class="title select" name="filterOpt" id="filterOpt">
-        <option value="">Latest </option>
-        <option value="trending">Popular</option>
-        <option value="newlyAdded">Newest</option>
-      </select>
-      <span class="title">Articles</span>
-    </div>
+    $getFollow = "SELECT * FROM fast_follows WHERE toUserID ='$UID'";
+    $followData = mysqli_query($link, $getFollow);
+    $followCount = mysqli_num_rows($followData);
 
-    <div class="posts cont500">
-      <div class="postBody">
-        <div class="postPic"> <img src="/uploads/posts/2022/7/12095427.jpg" alt=""> </div>
-        <div class="postTitle"><a href="" id="postTitle" href=""> How to get a full detail of your</a></div>
-        <div class="extFoot">
-          <span class="meta"><a id="channelName"  href="">Fast Hub</a></span>
-          <p class="dot">&#x2022;</p>
-          <span   class="meta"><a id="authorName" href="">Jhon Doe</a></span>
-          <p class="dot">&#x2022;</p>
-          <span id="pubTime" class="meta">1hr Ago</span>
+    $getRating = "SELECT * FROM fast_rating WHERE toUserID ='$UID'";
+    $rateData = mysqli_query($link, $getRating);
+    $rateCount = mysqli_num_rows($rateData);
+    if ($rateCount == 0) {
+      $rate = 0;
+    }else {
+      $totalRating = 0;
+      while ($row = $rateData->fetch_assoc()) {
+        $totalRating += $row['rateUser'];
+      }
+      $rateDec = $totalRating/$rateCount;
+      $rate = number_format((float)$rateDec, 1, '.','');
+    }
+
+    if ($upp == 0) {
+      $profileImage = "users/default.jpg";
+    }else {
+      $upp = unserialize($row['userProfilePic']);
+      $profileImage = $upp['folder'].'/'.$upp['year'].'/'.$upp['month'].'/'.$upp['id'].'.'.$upp['ext'];
+    }
+    $profile ='
+
+    '.$GLOBALS['containerStart'].'
+    <!-- Self Profile Opened -->
+      <div class="authorProfile">
+        <div class="topDiv">
+          <div class="authorPic"> <img src="../uploads/'.$profileImage.'" alt=""> </div>
+          <div class="authorDetails">
+            <div class="userNameWork">
+              <span id="userFullName">'.$userFullName.'</span>
+              <span id="userType">'.$userType.'</span>
+              <span class="designation">'.$userCountry.'</span>
+              <span class="designation">Joined '.$userJoinDate.'</span>
+            </div>
+            <div class="userParam">
+              <div class="userArticles">
+                <span class="userParameters">Articles</span>
+                <span class="values">'.$postsCount.'</span>
+              </div>
+              <div class="userFollowers">
+                <span class="userParameters">Follows</span>
+                <span class="values">'.$followCount.'</span>
+              </div>
+              <div class="userRating">
+                <span class="userParameters">Rating</span>
+                <span class="values">'.$rate.'</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bottomDiv">
+          <div id="linkOne"class="linkOne"> <span class="links"> <a href="../logout.php">Logout</a></span> </div>
+          <div id="linkTwo" class="linkOne"> <span class="links"> <a href="#">Settings</a> </span> </div>
         </div>
       </div>
-      <div class="postFooter">
-        <div class="footItems" id="react">
-          <div id="like" class="react"><img  class="footImages"  src="/assets/pics/svgs/thumbs-up.svg" alt=""></div>
-          <div id="likeCount" class="react rt footImages fontFam b sm ml_d4em">1</div>
-        </div>
-        <div class="footItems" >
-          <img id="comment"class="footImages"  src="/assets/pics/svgs/comment_notFilled_2.svg" alt="">
-          <div id="comentCount" class=" react rt footImages fontFam b sm ml_d4em">13</div>
-        </div>
-        <div class="footItems" id="share">
-          <img  class="footImages" src="/assets/pics/svgs/share_en.svg" alt="">
-        </div>
-      </div>
-    </div>
-  </div>
-<!-- Popular Articles Close -->';
-?>
-
+    <!-- Self Profile Closed -->'..'$GLOBALS['divStop']';
+  }
+  }
+ ?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -136,159 +173,8 @@ $articles = '<!-- Popular Articles Open -->
       <span> <a id="backArrow" href="../">&#171;  <span>Back</span></a> </span>
     </div>
     <?php
-    if(isset($_COOKIE['uisnnue'])) {
-      if (!empty($_COOKIE['uisnnue'])) {
-        include '../_.config/sjdhfjsadkeys.php';
-        $dUserID = openssl_decrypt ($_COOKIE['uisnnue'], $ciphering,
-        $decryption_key, $options, $decryption_iv);
-        $_SESSION["uisnnue"] = $dUserID;
-        if (checkUserID($_SESSION["uisnnue"])) {
-          renderProfile($_SESSION['uisnnue']);
-        }else {
-          echo '<script type="text/javascript">
-            document.location = "../login/?code=001UNF";
-          </script>';
-        }
-      }else {
-        echo '<script type="text/javascript">
-          document.location = "../login/?code=002UNF";
-        </script>';
-      }
-
-    }elseif (isset($_SESSION['uisnnue']) && isset($_GET['eikooCtes'])) {
-      if ((boolean)$_GET['eikooCtes']) {
-        if (checkUserID($_SESSION['uisnnue'])) {
-          $encUID = openssl_encrypt($_SESSION['uisnnue'], $ciphering,
-          $encryption_key, $options, $encryption_iv);
-          setcookie('uisnnue', $encUID, time()+(86400*30), '/');
-          $u = $_SESSION['uisnnue'];
-          renderProfile($u);
-        }else {
-          var_dump('user not exist');
-          // echo '<script type="text/javascript">
-          //   document.location = "../login?code=0003UNF";
-          // </script>';
-        }
-      }else {
-        if (checkUserID($_SESSION['uisnnue'])) {
-          renderProfile($_SESSION['uisnnue']);
-        }else {
-          echo '<script type="text/javascript">
-            document.location = "../login?code=004UNF";
-          </script>';
-        }
-      }
-    }else {
-      echo '<script type="text/javascript">
-        document.location = "../login?code=00NCSS";
-      </script>';
-    }
-
-function checkUserID($dUserID){
-  include '../_.config/_s_db_.php';
-  $link = new mysqli("$hostName","$userName","$passWord","$dbName");
-  $checkUserID = "SELECT userID FROM fast_users Where userID = '$dUserID'";
-  $userDat = mysqli_query($link, $checkUserID);
-  if (mysqli_num_rows($userDat)) {
-    $exist = true;
-  }else {
-    setcookie("uisnnue", "", time()-3600);
-    unset($_SESSION['uisnnue']);
-    $exist = false;
-  }
-  return $exist;
-}
-
-
-  function renderProfile($UID){
-    include '../_.config/_s_db_.php';
-    $link = new mysqli("$hostName","$userName","$passWord","$dbName");
-    $getUserData = "SELECT * FROM user_credentials Where userID = '$UID'";
-    $result = mysqli_query($db,$getUserData);
-    if (mysqli_num_rows($result)) {
-      $row = $result->fetch_assoc();
-      $userFullName = $row['userFullName'];
-      $userJoinDate = $row['userJoiningDate'];
-      $userCountry = $row['userCountry'];
-      switch ($row['userType']) {
-        case '0':
-          $userType = "Reader";
-          break;
-        case '1':
-          $userType = "Author";
-          break;
-        case '2':
-          $userType = "Administator";
-          break;
-        default:
-          $userType = "Reader";
-          break;
-      }
-      $getPost = "SELECT * FROM fast_posts WHERE userID ='$UID'";
-      $postData = mysqli_query($link, $getPost);
-      $postsCount = mysqli_num_rows($postData);
-
-      $getFollow = "SELECT * FROM fast_follows WHERE toUserID ='$UID'";
-      $followData = mysqli_query($link, $getFollow);
-      $followCount = mysqli_num_rows($followData);
-
-      $getRating = "SELECT * FROM fast_rating WHERE toUserID ='$UID'";
-      $rateData = mysqli_query($link, $getRating);
-      $rateCount = mysqli_num_rows($rateData);
-      if ($rateCount == 0) {
-        $rate = 0;
-      }else {
-        $totalRating = 0;
-        while ($row = $rateData->fetch_assoc()) {
-          $totalRating += $row['rateUser'];
-        }
-        $rateDec = $totalRating/$rateCount;
-        $rate = number_format((float)$rateDec, 1, '.','');
-      }
-
-      if ($upp == 0) {
-        $profileImage = "users/default.jpg";
-      }else {
-        $upp = unserialize($row['userProfilePic']);
-        $profileImage = $upp['folder'].'/'.$upp['year'].'/'.$upp['month'].'/'.$upp['id'].'.'.$upp['ext'];
-      }
-      echo $GLOBALS['containerStart'];
-      echo '
-      <!-- Self Profile Opened -->
-        <div class="authorProfile">
-          <div class="topDiv">
-            <div class="authorPic"> <img src="../uploads/'.$profileImage.'" alt=""> </div>
-            <div class="authorDetails">
-              <div class="userNameWork">
-                <span id="userFullName">'.$userFullName.'</span>
-                <span id="userType">'.$userType.'</span>
-                <span class="designation">'.$userCountry.'</span>
-                <span class="designation">Joined '.$userJoinDate.'</span>
-              </div>
-              <div class="userParam">
-                <div class="userArticles">
-                  <span class="userParameters">Articles</span>
-                  <span class="values">'.$postsCount.'</span>
-                </div>
-                <div class="userFollowers">
-                  <span class="userParameters">Follows</span>
-                  <span class="values">'.$followCount.'</span>
-                </div>
-                <div class="userRating">
-                  <span class="userParameters">Rating</span>
-                  <span class="values">'.$rate.'</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="bottomDiv">
-            <div id="linkOne"class="linkOne"> <span class="links"> <a href="../logout.php">Logout</a></span> </div>
-            <div id="linkTwo" class="linkOne"> <span class="links"> <a href="#">Settings</a> </span> </div>
-          </div>
-        </div>
-      <!-- Self Profile Closed -->';
-    }
-    echo $GLOBALS['divStop'];
+    if (isset($profile)) {
+      echo $profile;
     }
      ?>
   </body>
