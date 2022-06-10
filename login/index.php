7b8f7dacccd2ac8fcef342a23d0aa1697ec8cfc4
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-10 09:40:52 +0530

    fghgh

--- login/index.php
+++ login/index.php
@@ -8,7 +8,7 @@ if (isset($_GET['message'])) {
 }
 session_start();
 if (isset($_SESSION["uisnnue"]) || isset($_COOKIE["uisnnue"])) {
-  header("Location: ../profile.php");
+  header("Location: ../profile/");
 }
 ?>
 
