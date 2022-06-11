48fad74922e97f16c88dfeada5626b35cf305653
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-11 23:27:40 +0530

    ghjhgj

--- components/randVersion.php
+++ components/randVersion.php
@@ -1,34 +1,10 @@
 <?php
-include '../_.config/_s_db_.php';
-$sql = "SELECT * FROM fast_options WHERE opt_ID = '5648902096'";
-$res = mysqli_query($db, $sql);
-$row = $res->fetch_assoc();
-$version = (int)$row['opt_value'];
-$aV = array_map('intval', str_split($version));
-$var = 0;
-if ($version < 10) {
-  $ver = $aV[0].'.0.0.0.0';
-}elseif($version < 100) {
-  $ver = $aV[0].'.'.$aV[1].'.0.0.0';
-}elseif ($version < 1000) {
-  $ver = $aV[0].'.'.$aV[1].'.'.$aV[2].'.0.0';
-}elseif ($version < 10000) {
-  $ver = $aV[0].'.'.$aV[1].'.'.$aV[2].'.'.$aV[3].'.0';
-}elseif ($version < 100000) {
-  $ver = $aV[0].'.'.$aV[1].'.'.$aV[2].'.'.$aV[3].'.'.$aV[4];
-}else {
-  $ver = $version;
+if (!isset($_SESSION['randVersion'])) {
+  $r1= rand(0,10);
+  $r2= rand(0,10);
+  $r3= rand(0,10);
+  $r4 = rand(0,10);
+  $randVersion ="$r1"."."."$r2"."."."$r3".".".$r4;
+  $_SESSION['randVersion'] = $randVersion;
 }
-if (!isset($_COOKIE['cssVersion'])) {
-  setcookie('cssVersion',$ver, time()+(86400*7), '/');
-}else {
-  if ($version != $_COOKIE['cssVersion']) {
-    setcookie('cssVersion',$ver, time()+(86400*7), '/');
-  }else {
-    if (!isset($_SESSION['randVersion'])) {
-      $_SESSION['randVersion'] = $_COOKIE['cssVersion'];
-    }
-  }
-}
-
 ?>
