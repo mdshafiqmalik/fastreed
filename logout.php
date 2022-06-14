25a99e9f9c7e3b6ecc3eab167dcd06732defe3f1
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-14 16:57:08 +0530

    ghjhj

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@ session_start();
 setcookie('logID', '', time() -3600, "/");
 include '_.config/_s_db_.php';
 $logID = $_SESSION['logID'];
-echo $loginID;
+var_dump($loginID);
 $sql = "UPDATE `fast_logged_users` SET `status` = \'6\' WHERE `fast_logged_users`.`loginID` = '$logID'";
 if ($result = mysqli_query($db, $sql)) {
   unset($_SESSION['logID']);
