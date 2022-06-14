b30e6b5e6cee4f704aba621649b0e918cafab9e9
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-14 19:00:07 +0530

    gfhgh

--- login/index.php
+++ login/index.php
@@ -11,6 +11,7 @@ if (isset($_GET['message'])) {
 if (isset($_SESSION["logID"]) || isset($_COOKIE["uisnnue"])) {
   var_dump(true);
   if (!empty($_SESSION['logID'])) {
+    var_dump($_SESSION['logID']);
     // header("Location: ../profile/");
   }
 }
