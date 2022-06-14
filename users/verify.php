08d12cfddf1004e43aab312ae0f9de5a93c50438
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-14 18:07:47 +0530

    ghjh

--- users/verify.php
+++ users/verify.php
@@ -206,6 +206,20 @@ function createLogin($userID){
   return $loginCreated;
 }
 
+function random_str(
+    int $length = 64,
+    string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+): string {
+    if ($length < 1) {
+        throw new \RangeException("Length must be a positive integer");
+    }
+    $pieces = [];
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+        $pieces []= $keyspace[random_int(0, $max)];
+    }
+    return implode('', $pieces);
+}
 
 // Move User Data from no Verify to verified users
 function createUser($userID){
