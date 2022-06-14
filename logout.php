a217383f4b865a3d3d15ba16ec4d9d7af671e00e
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-14 17:43:21 +0530

    ghjhj

--- logout.php
+++ logout.php
@@ -3,14 +3,14 @@ session_start();
 $logID = $_COOKIE['logID'];
 include '_.config/_s_db_.php';
 var_dump($loginID);
-$sql = "UPDATE `fast_logged_users` SET `status` = \'6\' WHERE `fast_logged_users`.`loginID` = '$logID'";
-$result =mysqli_query($db, $sql);
-if ($result) {
-  unset($_SESSION['logID']);
-  session_destroy();
-  header("Location: login");
-}else {
-  var_dump("Cannot Logout");
-}
+// $sql = "UPDATE `fast_logged_users` SET `status` = \'6\' WHERE `fast_logged_users`.`loginID` = '$logID'";
+// $result =mysqli_query($db, $sql);
+// if ($result) {
+//   unset($_SESSION['logID']);
+//   session_destroy();
+//   header("Location: login");
+// }else {
+//   var_dump("Cannot Logout");
+// }
 
  ?>
