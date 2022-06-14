8905b2d11ac20d773357a685458f6185767cd1e6
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-14 17:05:21 +0530

    gfg

--- logout.php
+++ logout.php
@@ -5,9 +5,10 @@ include '_.config/_s_db_.php';
 $logID = $_COOKIE['logID'];
 var_dump($loginID);
 $sql = "UPDATE `fast_logged_users` SET `status` = \'6\' WHERE `fast_logged_users`.`loginID` = '$logID'";
+session_destroy();
 if ($result = mysqli_query($db, $sql)) {
   unset($_SESSION['logID']);
-  session_destroy();
+
   header("Location: login");
 }else {
   var_dump("Cannot Logout");
