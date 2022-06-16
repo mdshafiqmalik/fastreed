012d5d27ca92b76de4e047a0898a39f00b0d3f1c
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-16 16:36:51 +0530

    fggh

--- components/header.php
+++ components/header.php
@@ -16,10 +16,10 @@ $header = '<div class="header">
   </div>
 </div>';
 $subheader = '<div class="subheader">
-  <div><a href="/"><img id="home" class="submenu" src="/'.$svgFolder.'/home_NF.svg" alt=""></a></div>
+  <div><a href="/"><img id="home" class="submenu" src="/'.$svgFolder.'/home.svg" alt=""></a></div>
   <!--<div><a href="channels.php"><img id="subscribe" class="submenu" src="/'.$svgFolder.'/logo.svg" alt=""></a></div>-->
-  <div><a href="/notifications"><img id="notification" class="submenu" src="/'.$svgFolder.'/bell_NF.svg" alt=""></a> </div>
-  <div><a href="/'.$user.'/"><img id="user" class="submenu" src="/'.$svgFolder.'/user_notFilled.svg" alt=""></a> </div>
+  <div><a href="/notifications"><img id="notification" class="submenu" src="/'.$svgFolder.'/circle_notifications.svg" alt=""></a> </div>
+  <div><a href="/'.$user.'/"><img id="user" class="submenu" src="/'.$svgFolder.'/user.svg" alt=""></a> </div>
 </div>';
 $homeOther = ($_SERVER['REQUEST_URI'] == "/users/" || $_SERVER['REQUEST_URI'] == "/notifications/");
 if ($homeOther) {
