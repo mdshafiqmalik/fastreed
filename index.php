f7a108ff72f1116780b9d5a9b8e7c2d70b475694
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-17 08:32:16 +0530

    hgh

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ if (isset($_SESSION['loggedIn'])){
 }
  ?>
 <!DOCTYPE html>
-<html lang="hi" dir="ltr">
+<html dir="ltr">
   <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="robots" content="noindex">
