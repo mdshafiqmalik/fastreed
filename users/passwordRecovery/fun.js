ca07ffe18003920d9b68a024444556e51c8a60a8
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-21 13:16:30 +0530

    jl

--- users/passwordRecovery/fun.js
+++ users/passwordRecovery/fun.js
@@ -11,3 +11,49 @@ function change(){
     document.getElementById('password').type = "password";
   }
 }
+
+
+function checkFeild(){
+   var confPass =  document.getElementById('confirmPassword').value;
+    var newPass =  document.getElementById('newPassword').value;
+   var cnfP = document.getElementById('CNFP');
+   var confPass;
+   if (checkPass()) {
+     if (confPass.length < 8) {
+       cnfP.innerHTML = "Min. Password Length is 8";
+       cnfP.style.color = "red";
+       confPass = false;
+     }else {
+       if (newPass == confPass ) {
+         cnfP.innerHTML = "Password Matched";
+         cnfP.style.color = "green";
+         confPass = true;
+         submit.style.background = "#000";
+         submit.removeAttribute("disabled");
+       }else {
+         cnfP.innerHTML = "Password Not Matched";
+         cnfP.style.color = "red";
+         confPass = false;
+       }
+     }
+   }
+   function checkPass(){
+     var newPass =  document.getElementById('newPassword').value;
+     var nwP =document.getElementById('NWP');
+     var checkPass;
+     if (newPass.length < 8) {
+       nwP.innerHTML = "Min. Password Length is 8";
+       nwP.style.color = "red";
+       checkPass = false;
+     }else {
+       nwP.innerHTML = "Password Accepted";
+       nwP.style.color = "green";
+       checkPass = true;
+     }
+     return checkPass;
+  }
+
+}
+   var submit = document.getElementById('submitPass');
+   submit.style.background = "#aaa";
+ </script>
