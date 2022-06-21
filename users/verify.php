16a6af1b11beccf43e449db74d076e411b79f0f3
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-21 09:39:16 +0530

    hgh

--- users/verify.php
+++ users/verify.php
@@ -157,9 +157,9 @@ if (isset($_GET['_secRandID'])) {
   <head>
     <meta charset="utf-8">
     <?php include '../components/randVersion.php' ?>
-    <link rel="stylesheet" href="src/style.css?v=<?php echo $_SESSION['randVersion']; ?>">
-    <link rel="stylesheet" href="../assets/css/root.css?v=<?php echo $_SESSION['randVersion']; ?>">
-    <link rel="stylesheet" href="src/profile.css?v=<?php echo $_SESSION['randVersion']; ?>">
+    <link rel="stylesheet" href="src/style.css?v=<?php echo $randVersion; ?>">
+    <link rel="stylesheet" href="../assets/css/root.css?v=<?php echo $randVersion; ?>">
+    <link rel="stylesheet" href="src/profile.css?v=<?php echo $randVersion; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
   </head>
@@ -176,8 +176,8 @@ if (isset($_GET['_secRandID'])) {
     ?>
   </div>
   </div>
-  <script src="src/fun.js?v=<?php echo $_SESSION['randVersion'] ?>" charset="utf-8"></script>
-  <script src="../assets/js/jquery-3.6.0.js?v=<?php echo $_SESSION['randVersion'] ?>" charset="utf-8"></script>
+  <script src="src/fun.js?v=<?php echo $randVersion ?>" charset="utf-8"></script>
+  <script src="../assets/js/jquery-3.6.0.js?v=<?php echo $randVersion ?>" charset="utf-8"></script>
   </body>
 </html>
 
