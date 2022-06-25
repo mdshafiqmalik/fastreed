d2439d62238b678e653c976621033350624bfb06
Mohd Shafiq <mdshafiqmalik98@gmail.com>
2022-06-25 23:40:54 +0530

    gjhj

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ if (isset($_SESSION['logID'])) {
               <a id="profilelink" href="profile">View your profile</a>
             </span>
           </div>';
-      $GLOBALS['Menu'] = $loggedProfile.$writeArticles.$updateProfile.$channels.'<hr>'.$Interests.$Languages.$privacy.$securityLogin.'<hr>'.$termsPolicy.$helpFeedback.$aboutUs.$logout;
+      $GLOBALS['Menu'] = $loggedProfile."<div id='user'>".$writeArticles.$updateProfile.$channels.'</div>'.$Interests.$Languages.$privacy.$securityLogin.'<hr>'.$termsPolicy.$helpFeedback.$aboutUs.$logout;
     }else {
       $GLOBALS['Menu'] = $nonLoggedProfile.$login.$aboutUs.$Languages.$Interests.$privacy.$termsPolicy.$helpFeedback;
     }
